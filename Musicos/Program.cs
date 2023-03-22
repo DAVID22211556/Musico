2755e3ac22b9cbf48e686f756ba93f283202643b
DAVID22211556 <126517414+DAVID22211556@users.noreply.github.com>
2023-03-22 19:57:03 +0000

    rrr

---
+++ Musicos/Program.cs
@@ -0,0 +1,34 @@
+﻿class musico
+{
+    public string nombre{get; set;}
+    public musico(string n,string instrumento)
+    { 
+        nombre=n;
+        instrumento= instrumento;
+    }
+    public void saluda(){Console.WriteLine($"hola soy {nombre}");}
+
+
+}
+class bajista:musico
+{
+    public string bajo{get; set;}
+    public bajista(string n,string b):base(n)
+    {
+        bajo=b;
+    }
+    public override void toca()
+    {Console.WriteLine($"{nombre} tocando si {bajo} ");}
+}
+
+
+internal class Program
+{
+
+    List<musico> sodastereo = new List<musico>();
+    
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
